52131bee4da45484eee0c0eb36a011657d4ae41a
Suman Mishra <40954938+Sukun-S@users.noreply.github.com>
2020-05-21 18:07:10 +0530

    Create uc1.sh

---
+++ uc1.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash -x
+shopt -s extglob
+echo "Welcome to User Registration"
+echo "Enter the First Name"
+read FirstName
+FirstNamePat="^[A-Z]{1,}[a-z]*"
+if [[ $FirstName =~ $FirstNamePat ]];
+then
+	echo "Valid";
+else
+	echo "Invalid";
+fi
